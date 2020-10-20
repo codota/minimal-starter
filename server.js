84c975d2f91107c8249dceb00abc56cc490eebc9
Doron Atuar <doron@codota.com>
2020-10-20 09:33:30 +0300

    fixing semicolon

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('*', (req, res) => {
-  res.send('OK')
-})
+  res.send('OK');
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('App listening on port ' + listener.address().port)
-})
+  console.log('App listening on port ' + listener.address().port);
+});
